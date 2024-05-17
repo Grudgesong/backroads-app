9a81e98da7f9aa63bbfa741cc41c115a970ae853
Sangraj Ranjit <Grudgesong@gmail.com>
2024-05-17 23:25:18 +0800

    fixing hard coded social links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import logo from "../images/logo.svg";
-import { pageLinks } from "../data";
+import { pageLinks, socialLinks } from "../data";
 
 export default function Navbar() {
   return (
@@ -26,33 +26,20 @@ export default function Navbar() {
           </ul>
 
           <ul className="nav-icons">
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace"></i>
-              </a>
-            </li>
+            {socialLinks.map(({ id, href, icon }) => {
+              return (
+                <li key={id}>
+                  <a
+                    href={href}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="nav-icon"
+                  >
+                    <i className={icon}></i>
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </nav>
